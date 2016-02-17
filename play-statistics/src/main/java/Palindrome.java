3c917cf77095372b74358fd8f98ef5860f69d858
Sir Mythos <SirMythos@web.de>
2016-02-17 21:42:57 +0100

    Initial Upload

---
+++ play-statistics/src/main/java/Palindrome.java
@@ -0,0 +1,10 @@
+
+public class Palindrome {
+	
+	public static boolean isPalindrome(String input) {
+		String a = input.toLowerCase();
+		String b = new StringBuilder(a).reverse().toString();
+		return a.equals(b);
+	}
+
+}
---
+++ play-statistics/src/test/java/PalindromeTest.java
@@ -0,0 +1,16 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class PalindromeTest {
+	
+	@Test
+	public void isPalindromeTest() {
+		Assert.assertTrue(Palindrome.isPalindrome("Otto"), "Ist kein Palindrome");
+	}
+	
+	@Test
+	public void isNoPalindromeTest(){
+		Assert.assertFalse(Palindrome.isPalindrome("Test"), "Ist ein Palindrome");
+	}
+
+}
