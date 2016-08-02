05c3f05af96c995b260947417c75bbf0b6b03272
lutz.kramer <lutz.kramer@D3GM992-PC.bsh2.local>
2016-08-02 13:10:53 +0200

    Update Submodule

---
+++ src/main/java/de/sirmythos/wmh_tools/starter/WMH_Tools.java
@@ -0,0 +1,13 @@
+package de.sirmythos.wmh_tools.starter;
+
+import javax.swing.JOptionPane;
+
+public class WMH_Tools {
+
+	public static void main(String[] args) {
+
+		JOptionPane.showMessageDialog(null, "WMH-Tools mit Modulen");
+
+	}
+
+}
---
+++ wmh-tournaments/src/main/java/de/sirmythos/wmh_tournaments/starter/WMH_Tournaments.java
@@ -0,0 +1,13 @@
+package de.sirmythos.wmh_tournaments.starter;
+
+import javax.swing.JOptionPane;
+
+public class WMH_Tournaments {
+
+	public static void main(String[] args) {
+		
+		JOptionPane.showMessageDialog(null, "WMH-Tournaments");
+
+	}
+
+}
